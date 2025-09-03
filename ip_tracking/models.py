733334e4d26855fe08c12eeecfa4a047e3908166
Loutimi <loutimi59@gmail.com>
2025-09-03 14:20:43 +0100

    Add SuspiciousIP model.

--- ip_tracking/models.py
+++ ip_tracking/models.py
@@ -20,3 +20,15 @@ class BlockedIP(models.Model):
     Model to store blocked IP addresses.
     """
     ip_address = models.GenericIPAddressField(unique=True)
+
+
+class SuspiciousIP(models.Model):
+    """
+    Model to flag suspicious IPs.
+    """
+    ip_address = models.GenericIPAddressField()
+    reason = models.CharField(max_length=255)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.ip_address} - {self.reason}"
