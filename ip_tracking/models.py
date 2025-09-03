db3867da4e4212285e07f35dd69afe519b62136c
Loutimi <loutimi59@gmail.com>
2025-09-03 12:37:01 +0100

    Add RequesLog model to log IP addresses, timestamp and path of every incoming request.

--- ip_tracking/models.py
+++ ip_tracking/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class RequestLog(models.Model):
+    """
+    Model to log IP addresses, timestamp and path of
+    every incoming request.
+    """
+    ip_address = models.GenericIPAddressField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    path = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.ip_address} - {self.path} @ {self.timestamp}"
