838daaf20734fa071ac247ccebb766a9edd46ebe
Loutimi <loutimi59@gmail.com>
2025-09-03 14:19:57 +0100

    Apply the rate limit to LoginView.

--- ip_tracking/views.py
+++ ip_tracking/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.conf import settings
+from django_ratelimit.decorators import ratelimit
+from django.views import View
 
-# Create your views here.
+
+class LoginView(View):
+    def post(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            # Authenticated users → 10 requests/min
+            decorator = ratelimit(key="user", rate=settings.RATELIMITS["authenticated"], block=True)
+        else:
+            # Anonymous users → 5 requests/min
+            decorator = ratelimit(key="ip", rate=settings.RATELIMITS["anonymous"], block=True)
+
+        wrapped_view = decorator(
+            lambda req, *a, **kw: JsonResponse({"message": "Login attempt"})
+        )
+        return wrapped_view(request, *args, **kwargs)
